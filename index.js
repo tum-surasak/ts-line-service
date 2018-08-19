ed846283d2489fb473c794b6fab70190814b7bac
tum-surasak <surasak.rit@mahidol.edu>
2018-08-19 13:34:03 +0700

    no message

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require('express')
 const app = express()
+const { Client } = require('pg')
 const port = process.env.PORT || 4000
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  });
+app.get('/', (req, res) => res.sendStatus(200))
+app.get('/dataurl', (req, res) => {
+    res.setHeader('content-type', 'text/json')
+    res.send({
+        test: 5556
+    })
+})
 app.post('/webhook', (req, res) => res.sendStatus(200))
 app.listen(port)
