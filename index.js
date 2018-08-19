9fc0525cb7ff77ceda5c829021d767df46cd32e9
tum-surasak <surasak.rit@mahidol.edu>
2018-08-19 14:03:31 +0700

    [added] npm install [body-parser, request] --save [edited] index.js copy code from --> https://medium.com/ingkwan/%E0%B8%AA%E0%B8%A3%E0%B9%89%E0%B8%B2%E0%B8%87-line-bot-%E0%B8%94%E0%B9%89%E0%B8%A7%E0%B8%A2-node-js-aiml-a-beginners-guide-b7708b0b2440

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
+// Reply with two static messages
+
 const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
 const app = express()
-const { Client } = require('pg')
 const port = process.env.PORT || 4000
-const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
-  });
-app.get('/', (req, res) => res.sendStatus(200))
-app.get('/dataurl', (req, res) => {
-    res.setHeader('content-type', 'text/json')
-    res.send({
-        test: 5556
-    })
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.post('/webhook', (req, res) => {
+    let reply_token = req.body.events[0].replyToken
+    reply(reply_token)
+    res.sendStatus(200)
 })
-app.post('/webhook', (req, res) => res.sendStatus(200))
 app.listen(port)
+function reply(reply_token) {
+    let token = 'CJjG80i3woYXfPUF8ttj8FhCiESBGo4XxtaKmGmlG86gLzjGLamEBDopN4Hc64udxnfn4jUwwLV2UD6/woghTchN+RSZWCza2PQrIbyNXtEKg34/olmkGzrEdqtCJpavRuFgzJzR5nQpuIcXYBp1IQdB04t89/1O/w1cDnyilFU='
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
+    }
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [{
+            type: 'text',
+            text: 'Hello'
+        },
+        {
+            type: 'text',
+            text: 'How are you?'
+        }]
+    })
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
+    });
+}
