b446f7d6250e038518aadcc7e7d6d24475e7a688
tum-surasak <surasak.rit@mahidol.edu>
2018-08-26 23:49:56 +0700

    [edited] reply message.

--- reply.js
+++ reply.js
@@ -31,16 +31,19 @@ app.get('/db', (req,res) => {
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
     var db = firebase.database(),
-    current_path = 'ts-line-service'
-    newKey = db.ref(current_path).push().key
-    newObject = req.body.events[0]
+    current_path = 'ts-line-service',
+    newKey = db.ref(current_path).push().key,
+    newObject = req.body.events[0],
+    message = newObject.message
+
     db.ref(current_path + '/' + newKey).set(newObject)
-    reply(reply_token)
+    if(message.text.indexOf('/bot') === 0)    
+        reply(message.text, reply_token)
     //res.send(reply_token);
     res.sendStatus(200)
 })
 app.listen(port, () => console.log('http://localhost:' + port + '/db'))
-function reply(reply_token) {
+function reply(cmd, reply_token) {
     let token = 'CJjG80i3woYXfPUF8ttj8FhCiESBGo4XxtaKmGmlG86gLzjGLamEBDopN4Hc64udxnfn4jUwwLV2UD6/woghTchN+RSZWCza2PQrIbyNXtEKg34/olmkGzrEdqtCJpavRuFgzJzR5nQpuIcXYBp1IQdB04t89/1O/w1cDnyilFU='
     let headers = {
         'Content-Type': 'application/json',
@@ -52,6 +55,9 @@ function reply(reply_token) {
             "type": "sticker",
             "packageId": "1",
             "stickerId": "1"
+        },{
+            "type": "text",
+            "text": cmd
         }]
     })
     request.post({
